4a810c8bd3c75af15437bfe9938771570f2bed81
Jakub Arnold <darthdeus@gmail.com>
2017-08-05 23:44:42 +0200

    Add project files.

---
+++ ToM-Donations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToM_Donations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
