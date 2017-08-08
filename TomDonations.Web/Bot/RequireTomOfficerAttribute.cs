a0cd760d9d5b716991c88b1ed96ded2b2fb35a44
Jakub Arnold <darthdeus@gmail.com>
2017-08-08 03:44:29 +0200

    Only allow TT to use commands

--- TomDonations.Web/Bot/RequireTomOfficerAttribute.cs
+++ TomDonations.Web/Bot/RequireTomOfficerAttribute.cs
@@ -7,7 +7,11 @@ namespace TomDonations.Web {
         public async override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services) {
             Console.WriteLine($"Username: {context.User.Username}, Discriminator: {context.User.Discriminator}, DValue: {context.User.DiscriminatorValue}");
 
-            return PreconditionResult.FromSuccess();
+            if (context.User.Username == "Jay.Cee") {
+                return PreconditionResult.FromSuccess();
+            } else {
+                return PreconditionResult.FromError("You are not allowed to modify donation points.");
+            }
         }
     }
 }
